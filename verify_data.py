38a22a5904962842194e110ca84c714f9840176f
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-03-08 01:17:52 +1000

    Improved verify functions

--- verify_data.py
+++ verify_data.py
@@ -1,20 +1,9 @@
 # Creation Date: 06/03/2022
 
 import pandas
-import sys
 import ast
-import os
 
 pandas.options.mode.chained_assignment = None  # default='warn'
-
-
-def halt():
-    print("\nInterrupted")
-    try:
-        sys.exit(0)
-    except SystemExit:
-        os._exit(0)
-
 
 def build_dataframe(input_name: str):
     ''' Returns: Built dataframe structure. '''
@@ -34,7 +23,7 @@ def show_data(row: list, country: str = "AU"):
         Seek: {row["seek_url"]} Scores: {scores[1]}')
 
 
-def user_input(row, index, allow_n: bool = True):
+def user_input(row, index: int, allow_n: bool = True):
     ''' Returns valid user command line argument. '''
     show_data(row)
     valid_values = ("0", "1", "n") if allow_n and index != 0 else ("0", "1")
@@ -51,15 +40,14 @@ def user_input(row, index, allow_n: bool = True):
     return correct
 
 
-def fix_current(dataframe, correct, index, row):
-    ''' Returns Dataframe with current value fixed. '''
+def fix_current(dataframe, correct: str, index: int):
+    ''' Returns: Dataframe with current value fixed. '''
     text = "Correct" if bool(int(correct)) else "Incorrect"
     dataframe['correct'][index] = text
-    previous = [row, index]
-    return dataframe, previous
+    return dataframe
 
 
-def fix_previous(dataframe, previous):
+def fix_previous(dataframe, previous: list):
     ''' Returns: Dataframe with previous value fixed. '''
     print(f"Previous marked wrong, revisiting row {previous[1]}")
     correct = user_input(previous[0], previous[1], False)
@@ -84,14 +72,13 @@ def verify_data(input_name: str):
                     break
                 correct = user_input(row, index)
                 if correct != "n":
-                    dataframe, previous = fix_current(
-                        dataframe, correct, index, row)
+                    dataframe = fix_current(dataframe, correct, index)
+                    previous = [row, index]
                     break
                 dataframe = fix_previous(dataframe, previous)
     except KeyboardInterrupt:
         print(f'\nStopping at row {index}')
     dataframe.to_csv(input_name, index=False)
-
 
 
 verify_data("AUS_ONLY_1001+_Result")
