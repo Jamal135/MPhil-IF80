a79e3e4b8acdd31c1f961012af77d7a4ad305e89
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-06-29 19:14:25 +1000

    Fixed verification

--- verification/verification.py
+++ verification/verification.py
@@ -2,9 +2,7 @@
 
 
 import os
-import sys
 import keyboard
-import numpy as np
 import pandas as pd
 from time import sleep
 from dotenv import load_dotenv
@@ -17,8 +15,6 @@ def load_CSV(filename: str, drop_list: list = None):
     ''' Returns: CSV loaded to dataframe with select columns dropped. '''
     if not filename.endswith(".csv"):
         filename += ".csv"
-    if drop_list is None:
-        drop_list = []
     dataframe = pd.read_csv(filename)
     if drop_list is not None:
         dataframe.drop(drop_list, axis=1, inplace=True)
@@ -52,9 +48,16 @@ def linkedin_login(linkedin, auto_login: bool):
     sleep(5)
 
 
+def isNaN(num):
+    return num != num
+
+
 def start_browser(browser_name: str):
     ''' Returns: Selenium browser session. '''
-    browser_name = webdriver.Chrome(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    browser_name = webdriver.Chrome(ChromeDriverManager().install(),
+                                    chrome_options=options)
     browser_name.implicitly_wait(5)
     return browser_name
 
@@ -77,48 +80,93 @@ def update_windows(browsers: list, urls: list):
     ''' Purpose: Update Selenium sessions with new pages. '''
     for index, browser in enumerate(browsers):
         browser.get(urls[index])
-    sleep(5)
 
 
-def user_input(index, website):
+def user_input(index: int, column: str):
     ''' Purpose: Collect user input. '''
     while True:
         if keyboard.read_key() == "a":
-            print(f"Marked row {index}, {website} false...")
+            print(f"Marked row {index}, {column} false...")
             answer = 0
             break
         if keyboard.read_key() == "d":
-            print(f"Marked row {index}, {website} correct...")
+            print(f"Marked row {index}, {column}, correct...")
             answer = 1
             break
     return answer
-       
 
-def verify_data(filename: str, website: str, auto_login: bool = True):
+
+def save_CSV(df, filename: str, drop_list: list = None):
+    ''' Purpose: Saves new dataframe to CSV. '''
+    if not filename.endswith(".csv"):
+        filename += ".csv"
+    if drop_list is not None:
+        df.drop(drop_list, axis=1, inplace=True)
+    df.to_csv(filename, index=False)
+
+
+def verify_linkedin(filename: str, start: int = None, auto_login: bool = True,
+                    show_error: bool = False):
+    ''' Purpose: Verifies that the LinkedIn pages are accurate. '''
+    df = load_CSV(filename)
+    column = 'linkedin_bool'
+    if column not in df:
+        df[column] = 2
+    linkedin = start_browser("linkedin")
+    indexs = build_locations(df)
+    linkedin_login(linkedin, auto_login)
+    print("Checking LinkedIn...")
+    print("Press 'a' for incorrect and 'd' for correct...")
+    try:
+        for row in df.itertuples():
+            print("")
+            company = row[indexs['name']]
+            print(f"Checking {company}, row {row.Index}...")
+            if row[indexs[column]] in [0, 1] and row.Index != start:
+                print(f"Row {row.Index} already checked...")
+                continue
+            linkedin_url = f"https://www.{row[indexs['linkedin_url']]}"
+            update_windows([linkedin], [linkedin_url])
+            user_answer = user_input(row.Index, column)
+            df.at[row.Index, column] = user_answer
+    except (Exception, KeyboardInterrupt) as e:
+        if show_error:
+            print(e)
+    save_CSV(df, filename)
+
+
+def verify_website(filename: str, website: str, start: int = None, 
+                auto_login: bool = True, show_error: bool = False):
     ''' Purpose: Facilitates verification of links and stores status. '''
     df = load_CSV(filename)
     column = f'{website}_bool'
     if column not in df:
-        df[column] = 0
+        df[column] = 2
     browsers, indexs = start_session(df)
     linkedin_login(browsers[0], auto_login)
-    print("Press 'a' for correct and 'd' for incorrect.")
+    print(f"Checking {website}...")
+    print("Press 'a' for incorrect and 'd' for correct...")
     try:
         for row in df.itertuples():
-            if row[indexs[column]] == 1:
+            print("")
+            company = row[indexs['name']]
+            print(f"Checking {company}, row {row.Index}...")
+            if row[indexs[column]] in [0, 1] and row.Index != start:
+                print(f"Row {row.Index} already checked...")
                 continue
-            reviews_url =  row[indexs[f'{website}_url']]
-            if reviews_url is None:
-                row[indexs[column]] == 1
+            reviews_url = row[indexs[f'{website}_url']] # Add code to ensure it loads to Australia specifically
+            if isNaN(reviews_url):
+                df.at[row.Index, column] = 0
+                print(f"No valid {website} url...")
                 continue
-            linkedin_url = f"https://www.{row[indexs['linkedin_url']]}" 
+            linkedin_url = f"https://www.{row[indexs['linkedin_url']]}"
             update_windows(browsers, [linkedin_url, reviews_url])
-            user_answer = user_input(row.Index, website)
+            user_answer = user_input(row.Index, column)
             df.at[row.Index, column] = user_answer
-            print(df[row.Index, column])
-    except KeyboardInterrupt:
-        pass
-    #save
+    except (Exception, KeyboardInterrupt) as e:
+        if show_error:
+            print(e)
+    save_CSV(df, filename)
 
-
-verify_data('links/New_AUS_1001+_Links', 'indeed')
+# Need verification just for LinkedIn
+verify_linkedin('verification/AUS_1001+_Checking')
